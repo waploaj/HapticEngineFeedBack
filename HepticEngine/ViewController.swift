d12de55caa91aa5e6f9f3e87f86f6f39ff800773
Abdallah Abdillah <waploaj@icloud.com>
2022-09-10 11:39:17 +0300

    Adding Haptic Engine Feedback on ButtonClicked

--- HepticEngine/ViewController.swift
+++ HepticEngine/ViewController.swift
@@ -12,6 +12,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let buttonX = 150
+        let buttonY = 150
+        let buttonHeight = 50
+        let buttonWidth = 100
+        
+        let button = UIButton(type: .system)
+        button.setTitle("Click me!!", for: .normal)
+        button.tintColor = .blue
+        button.backgroundColor = .red
+        button.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
+        
+        
+        button.frame = CGRect(x: buttonX, y: buttonY, width: buttonWidth, height: buttonHeight)
+        
+        self.view.addSubview(button)
+    }
+    
+    let impact = UIImpactFeedbackGenerator()
+    
+    @objc func buttonClicked(sender:UIButton){
+        let alert = UIAlertController(title: "clicked", message: "you have clicked the button", preferredStyle: .alert)
+        self.present(alert, animated: true, completion:nil)
+        
+        impact.impactOccurred()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 
